f6bd0966717f5d98ba2de37a80ff8cecec945b29
ryanscott906 <rmscott456@gmail.com>
2020-02-22 21:58:20 -0600

    trying to sync with Heroku

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const db = require("./models");
-process.env.MONGODB_URI = 'mongodb://<user1>:<password1>@ds217548.mlab.com:17548/heroku_st2wrcpc'
 const PORT = process.env.PORT || 3000;
 
 const app = express();
